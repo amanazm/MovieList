1678ae0593e74156a8e61cbb804b6749cb1310f6
Aman <60620078+amanazm@users.noreply.github.com>
2021-08-27 13:52:12 +0530

    paging added

--- main.js
+++ main.js
@@ -4,14 +4,47 @@ const API_URL = BASE_URL+ '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
-const main=document.getElementById('main');
+const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('search');
+
+const prev =document.getElementById('prev');
+const current =document.getElementById('current');
+const next=document.getElementById('next');
+
+var currentPage =1;
+var nextPage =2;
+var prevPage=3;
+var lastUrl='';
+var totalPage=100;
+
+
 getMovies(API_URL);
-function getMovies(url){
-    fetch(url).then(res => res.json()).then(data =>{
-        console.log(data);
-        showMovies(data.results);
-    })
-}
+function getMovies(url) {
+    lastUrl = url;
+      fetch(url).then(res => res.json()).then(data => {
+          console.log(data.results)
+              showMovies(data.results);
+              currentPage =data.page;
+              nextPage=currentPage+1;
+              prevPage=currentPage-1;
+              totalPage=data.total_pages;
+
+              current.innerText=currentPage;
+
+              if(currentPage<=1){
+                  prev.classList.add('disabled');
+                  next.classList.remove('disabled');
+              }else if(currentPage>=totalPage){
+                prev.classList.remove('disabled');
+                next.classList.add('disabled');
+              }else{
+                prev.classList.remove('disabled');
+                next.classList.remove('disabled');
+              }
+      })
+  
+  }
 
 function showMovies(data){
     main.innerHTML='';
@@ -21,23 +54,15 @@ function showMovies(data){
         const movie1 = document.createElement('div');
         movie1.classList.add('movie');
         movie1.innerHTML=`
-        <div class="movie-info">
-        <div>
-        <center><img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}"></center>
-        </div>
-        <br/>
-        <div style="display:flex;">
-        <div style="width:80%">${title}</div>
-        <div>
-            <span class="${getColor(vote_average)}">${vote_average}</span>
-        </div>
-        </div>
-        </div>
-        
-        <div class="overview">
-            <h3>Overview</h3>
-            ${overview};
-        </div>
+        <img src="${poster_path? IMG_URL+poster_path: "http://via.placeholder.com/1080x1580" }" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getColor(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
         
         `
 
@@ -45,6 +70,8 @@ function showMovies(data){
     })
 
 }
+
+
  
 function getColor(vote){
     if(vote>= 8){
@@ -53,5 +80,46 @@ function getColor(vote){
         return 'orange';
     }else{
         return 'red';
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    if(searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+
+})
+
+prev.addEventListener('click',() =>{
+    if(nextPage>0){
+        pageCall(prevPage);
+    }
+})
+
+next.addEventListener('click',() =>{
+    if(nextPage<= totalPage){
+        pageCall(nextPage);
+    }
+})
+
+function pageCall(page){
+    let urlsplit = lastUrl.split('?');
+    let queryParams = urlsplit[1].split('&');
+    let key =queryParams[queryParams.length-1].split('=');
+    if(key[0] != 'page'){
+        let url = lastUrl+'&page=' +page
+        getMovies(url);
+    }else{
+        key[1]=page.toString();
+        let a= key.join('=');
+        queryParams[queryParams.length-1]=a;
+        let b= queryParams.join('&');
+        let url = urlsplit[0]+'?'+ b
+        getMovies(url);
     }
 }
